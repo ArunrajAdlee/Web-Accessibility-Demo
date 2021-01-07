4f5ec0dd9de7ecec9c7a03b2adad806d186f190d
a_adlee <arunraj.adlee@hotmail.com>
2021-01-07 00:02:03 -0500

    Modify 404page

--- frontend/src/components/Misc/404page.js
+++ frontend/src/components/Misc/404page.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSadTear, faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Error404 = () => {
   return (
     <>
-      <h1>404</h1>
-      <h4>Page not found!</h4>
+      <div className="error-page">
+        <FontAwesomeIcon className="error-image" icon={faSadTear} />
+        <h1>404</h1>
+        <h4>An error occured</h4>
+        <h5>Either go back or hit the header text to go to the home page!</h5>
+      </div>
     </>
   );
 }
